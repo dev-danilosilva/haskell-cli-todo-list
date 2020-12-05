53212945f8461a45c2fbed54bc3ebe4aee003ef0
Danilo Silva <danilo_silva@ethercity.tech>
2020-12-05 19:50:47 -0300

    Add, Update, View and Remove Parsers

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,38 @@ module Main where
 
 import Options.Applicative hiding (infoParser)
 
-type ItemIndex = Int
 
+data Options = Options FilePath Command deriving Show
+
+type ItemIndex       = Int
+type ItemTitle       = String
 type ItemDescription = Maybe String
+type ItemPriority    = Maybe String
+type ItemDueBy       = Maybe String
 
-data Options = Options FilePath Command deriving (Show)
+data ItemUpdate = ItemUpdate
+    { titleUpdate       :: Maybe ItemTitle
+    , descriptionUpdate :: Maybe ItemDescription
+    , priorityUpdate    :: Maybe ItemPriority
+    , dueByUpdate       :: Maybe ItemDueBy
+    } deriving Show
+
+data Item = Item
+    { title       :: ItemTitle
+    , description :: ItemDescription
+    , priority    :: ItemPriority
+    , dueBy       :: ItemDueBy
+    } deriving Show
+
 
 data Command
     = Info
     | Init
-    | Add
-    | View
+    | Add        Item
+    | View       ItemIndex
     | List
-    | Update
-    | Remove
+    | Update     ItemIndex ItemUpdate
+    | Remove     ItemIndex
     | PathCheck
     deriving Show
 
@@ -29,22 +47,37 @@ initParser :: Parser Command
 initParser = pure Init
 
 addParser :: Parser Command
-addParser = pure Add
+addParser = Add <$> addItemParser
+
+addItemParser :: Parser Item
+addItemParser = Item
+    <$> argument str (metavar "TITLE" <> help "Item Title")
+    <*> optional itemDescriptionValueParser
+    <*> optional itemPriorityValueParser
+    <*> optional itemDueByValueParser
 
 viewParser :: Parser Command
-viewParser = pure View
-
-updateParser :: Parser Command
-updateParser = pure Update
+viewParser = View <$> itemIndexParser
 
 removeParser :: Parser Command
-removeParser = pure Remove
+removeParser = Remove <$> itemIndexParser
 
 listParser :: Parser Command
 listParser = pure List
 
 pathCheckParser :: Parser Command
 pathCheckParser = pure PathCheck
+
+
+updateParser :: Parser Command
+updateParser = Update <$> itemIndexParser <*> updateItemParser
+
+updateItemParser :: Parser ItemUpdate
+updateItemParser = ItemUpdate
+    <$> optional updateItemTitleParser
+    <*> optional updateItemDescriptionParser
+    <*> optional updateItemPriorityParser
+    <*> optional updateItemDueByParser
 
 commandParser :: Parser Command
 commandParser = subparser $
@@ -66,36 +99,72 @@ dataPathParser :: Parser FilePath
 dataPathParser = strOption $
     value defaultDataPath
     <> long    "data-path"
-    <> short   'p'
+    <> short   'P'
     <> metavar "DATAPATH"
     <> help    ("Path to the data file (default " ++ defaultDataPath ++ ")")
 
-itemIndexParser :: Parser ItemIndex
-itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "Index of an item")
 
 itemDescriptionValueParser :: Parser String
 itemDescriptionValueParser = strOption $
        long     "desc"
     <> short    'd'
-    <> metavar  "ITEMDESCRIPTION"
+    <> metavar  "DESCRIPTION"
     <> help     "Item Description"
+
+itemTitleValueParser :: Parser String
+itemTitleValueParser = strOption $
+        long    "title"
+    <>  short   't'
+    <>  metavar "TITLE"
+    <>  help    "Item Title"
+
+itemDueByValueParser :: Parser String
+itemDueByValueParser = strOption $
+        long    "due-by"
+    <>  short   'b'
+    <>  metavar "DUEBY"
+    <>  help    "Due-By"
+
+itemPriorityValueParser :: Parser String
+itemPriorityValueParser = strOption $
+        long    "priority"
+    <>  short   'p'
+    <>  metavar "PRIORITY"
+    <>  help    "Item Priority"
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "Index of an item")
+
+updateItemTitleParser :: Parser ItemTitle
+updateItemTitleParser = itemTitleValueParser
+
+
+updateItemPriorityParser :: Parser ItemPriority
+updateItemPriorityParser =
+        Just <$> itemPriorityValueParser
+    <|> flag' Nothing (long "clear-priority")
 
 updateItemDescriptionParser :: Parser ItemDescription
 updateItemDescriptionParser =
         Just <$> itemDescriptionValueParser
-    <|> flag' Nothing (long "clear-desc")
+    <|> flag' Nothing (long "clear-priority")
+
+updateItemDueByParser :: Parser ItemDueBy
+updateItemDueByParser =
+        Just <$> itemDueByValueParser
+    <|> flag' Nothing (long "clear-due-by")
 
 
 run :: FilePath -> Command -> IO ()
 run filePath comm = case comm of
-    Info      -> print "Loading Info..."
-    Init      -> print "Initializing..."
-    Add       -> print "Adding To Do..."
-    List      -> print "Listing Items..."
-    Remove    -> print "Removing Item..."
-    Update    -> print "Updating Item..."
-    View      -> print "Loading View..."
-    PathCheck -> print filePath
+    Info                   -> print   "Loading Info..."
+    Init                   -> print   "Initializing..."
+    Add item               -> print $ "Add: item=" <> show item
+    List                   -> print   "Listing Items..."
+    Remove itemIndex       -> print $ "Removing Item..." <> show itemIndex
+    Update idx itemUpdate  -> print $ show idx <> " " <> show itemUpdate
+    View item              -> print $ "Loading Item " <> show item
+    PathCheck              -> print filePath
 
 main :: IO ()
 main =do
